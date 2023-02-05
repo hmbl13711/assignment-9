39f0951f6042356d83343b286e557d3afd82e28d
Hafiz-Bilal <hmbl13711@gmail.com>
2023-02-05 21:16:55 +0500

    update

--- app.ts
+++ app.ts
@@ -1,4 +1,214 @@
-// 1. Write a ts program to find length of a string.
-var str1: string="local"
-var str2: any= str1.split("")
-console.log(str2.length)
+// 1. Write a TypeScript program to list the properties of a TypeScript object
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+
+var student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12
+};
+
+for (let prop in student) {
+  console.log(prop);
+}
+
+// 2. Write a TypeScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+ var student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12
+};
+
+console.log("Object before deleting the property: ", student);
+
+delete student.rollno;
+
+console.log("Object after deleting the property: ", student);
+
+
+// 3. Write a TypeScript program to get the length of a TypeScript object.   
+// Sample object :
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+var student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12
+};
+
+let count = 0;
+for (let prop in student) {
+  count++;
+}
+
+console.log("Length of the object: ", count);
+
+ 
+
+// 4. Write a TypeScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.   
+
+// var library = [ 
+//    {
+//        author: 'Bill Gates',
+//        title: 'The Road Ahead',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Steve Jobs',
+//        title: 'Walter Isaacson',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Suzanne Collins',
+//        title:  'Mockingjay: The Final Book of The Hunger Games', 
+//        readingStatus: false
+//    }];
+ var library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false
+  }
+];
+
+for (let book of library) {
+  console.log("Book: " + book.title + " by " + book.author);
+  if (book.readingStatus) {
+    console.log("Reading status: Already read");
+  } else {
+    console.log("Reading status: Not read yet");
+  }
+  console.log("");
+}
+
+
+// 5. Write a TypeScript program to get the volume of a Cylinder with four decimal places using object classes.   
+// Volume of a cylinder : V = πr2h
+// where r is the radius and h is the height of the cylinder.
+class Cylinder {
+  private radius: number;
+  private height: number;
+
+  constructor(radius: number, height: number) {
+    this.radius = radius;
+    this.height = height;
+  }
+
+  getVolume(): number {
+    return Math.PI * Math.pow(this.radius, 2) * this.height;
+  }
+}
+
+const cylinder = new Cylinder(5, 10);
+const volume = cylinder.getVolume().toFixed(4);
+console.log(`Volume of the cylinder is: ${volume}`);
+
+ 
+
+// 6. Write a Bubble Sort algorithm in TypeScript.   
+// Note : Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted,
+// Sample Data: [6,4,0, 3,-2,1]
+// Expected Output : [-2, 0, 1, 3, 4, 6]
+ 
+
+// 7. Write a TypeScript program which returns a subset of a string.   
+// Sample Data: dog
+// Expected Output: ["d", "do", "dog", "o", "og", "g"]
+ 
+
+// 8. Write a TypeScript program to create a Clock.   
+// Note: The output will come every second.
+// Expected Console Output :
+// "14:37:42"
+// "14:37:43"
+// "14:37:44"
+// "14:37:45"
+// "14:37:46"
+// "14:37:47"
+ 
+
+// 9. Write a TypeScript program to calculate the area and perimeter of a circle.   
+// Note : Create two methods to calculate the area and perimeter. The radius of the circle will be supplied by the user.
+ 
+
+// 10. Write a TypeScript program to sort an array of TypeScript objects.   
+// Sample Object :
+
+// var library = [ 
+//    {
+//        title:  'The Road Ahead',
+//        author: 'Bill Gates',
+//        libraryID: 1254
+//    },
+//    {
+//        title: 'Walter Isaacson',
+//        author: 'Steve Jobs',
+//        libraryID: 4264
+//    },
+//    {
+//        title: 'Mockingjay: The Final Book of The Hunger Games',
+//        author: 'Suzanne Collins',
+//        libraryID: 3245
+//    }];
+// Expected Output:
+
+// [[object Object] {
+//   author: "Walter Isaacson",
+//   libraryID: 4264,
+//   title: "Steve Jobs"
+// }, [object Object] {
+//   author: "Suzanne Collins",
+//   libraryID: 3245,
+//   title: "Mockingjay: The Final Book of The Hunger Games"
+// }, [object Object] {
+//   author: "The Road Ahead",
+//   libraryID: 1254,
+//   title: "Bill Gates"
+// }]
+ 
+
+// 11. Write a TypeScript function to print all the methods in an TypeScript object.   
+// Test Data :
+// console.log(all_properties(Array));
+// ["length", "name", "arguments", "caller", "prototype", "isArray", "observe", "unobserve"]
+
+ 
+// 12. Write a TypeScript function to parse an URL.   
+ 
+
+// 13. Write a TypeScript function to retrieve all the names of object's own and inherited properties.   
+ 
+
+// 14. Write a TypeScript function to retrieve all the values of an object's properties.   
+ 
+
+// 15. Write a TypeScript function to convert an object into a list of `[key, value]` pairs.   
+ 
+
+// 16. Write a TypeScript function to get a copy of the object where the keys have become the values and the values the keys.   
+ 
+
+// 17. Write a TypeScript function to check whether an object contains given property.   
+ 
+
